dec7223b76314a8d2a3718fe42cece2d7c71ca8b
Ficus <1985973051@qq.com>
2021-12-16 11:21:59 +0800

    项目初始化 -引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
   <div id="app">
     <router-view/>
     <h1>Ficus头条</h1>
+    <div>
+      <i  class="toutiao toutiao-wenda"></i>
+    </div>
+    <van-button type="default">默认按钮</van-button>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 加载 Vant 核心组件库
+import Vant from 'vant'
+// 加载 Vant 全局样式
+import 'vant/lib/index.css'
 // 加载全局样式
 import './styles/index.less'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(Vant).mount('#app')
